648073f93a19a37579ee742286904ae2a80013d1
MonRoberto <roberto.quiroz117@outlook.com>
2024-12-03 22:32:53 -0600

    Seleccionar Elementos con querySelectorAll y getElementById

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,15 @@ heading.textContent = 'Nuevo Heading';
 console.log(heading);
 
 // querySelectorAll
+const enlaces = document.querySelectorAll('.navegacion a');
+console.log(enlaces[0]);
 
-
+enlaces[0].textContent = 'Nuevo Texto para Enlace';
+enlaces[0].classList.add('nueva-clase');
+enlaces[0].classList.remove('navegacion__enlace');
+// enlaces[0].href = 'http://google.com';
 
 // getElementById
+
+const heading2 = document.getElementById('heading');
+console.log(heading2);
