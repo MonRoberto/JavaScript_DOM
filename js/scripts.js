44c87e867b16a9c2fce7f7b66f2472fe6a70e9b8
MonRoberto <roberto.quiroz117@outlook.com>
2024-12-03 22:10:46 -0600

    Seleccionar Elementos con querySelector

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// querySelector
+const heading = document.querySelector('.header__texto h2') // Retorna 0 o 1 Elementos
+heading.textContent = 'Nuevo Heading';
+console.log(heading);
+
+// querySelectorAll
+
+
+
+// getElementById
