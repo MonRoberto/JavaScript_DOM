71e3f14f191c60b0c7e8d4bf7a16cb7bf5777f1b
MonRoberto <roberto.quiroz117@outlook.com>
2024-12-06 16:07:02 -0600

    Eventos en Javascript

--- js/scripts.js
+++ js/scripts.js
@@ -31,3 +31,22 @@ const navegacion = document.querySelector('.navegacion');
 navegacion.appendChild(nuevoEnlace);
 
 console.log(nuevoEnlace);
+
+
+// Eventos
+
+console.log(1);
+
+window.addEventListener('load', function() { // load espera a que el JS y los archivos que dependen del HTML estén listos
+    console.log(2);
+})
+
+window.onload = function() {
+    console.log(3);
+}
+
+document.addEventListener('DOMContentLoaded', function() { // Solo espera por el HTML, pero no espera CSS o imagenes
+    console.log(4);
+})
+
+console.log(5);
