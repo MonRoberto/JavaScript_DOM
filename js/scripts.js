80e750ebc68a4f0feb77fa815ce61b2f1efb307e
MonRoberto <roberto.quiroz117@outlook.com>
2024-12-06 15:41:40 -0600

    Crear HTML con createElement

--- js/scripts.js
+++ js/scripts.js
@@ -9,10 +9,25 @@ console.log(enlaces[0]);
 
 enlaces[0].textContent = 'Nuevo Texto para Enlace';
 enlaces[0].classList.add('nueva-clase');
-enlaces[0].classList.remove('navegacion__enlace');
+// enlaces[0].classList.remove('navegacion__enlace');
 // enlaces[0].href = 'http://google.com';
 
 // getElementById
 
 const heading2 = document.getElementById('heading');
 console.log(heading2);
+
+// Generar un nuevo enlace
+const nuevoEnlace = document.createElement('A')
+
+// Agregar el href
+nuevoEnlace.href = 'nuevo-enlace.html';
+// Agregar el texto
+nuevoEnlace.textContent = 'Un Nuevo Enlace';
+// Agregar la clase
+nuevoEnlace.classList.add('navegacion__enlace');
+// Agregarlo al documento
+const navegacion = document.querySelector('.navegacion');
+navegacion.appendChild(nuevoEnlace);
+
+console.log(nuevoEnlace);
